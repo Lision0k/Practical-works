7d940c3fe1215af2434d5f3f1620d15d4c72a4e9
Lision0k <ya.lizi.pav@gmail.com>
2025-02-28 13:58:10 +0300

    Practical work № 7, option 7
    7.Создать массив объектов House с полями: Адрес, Этаж, Количество комнат, Площадь. Создать массив объектов. Вывести: 
    а) список квартир, имеющих заданное число комнат; 
    б) список квартир, имеющих заданное число комнат и расположенных на заданном этаже;
    в) список квартир, имеющих площадь, превосходящую заданную.

--- Main.java
+++ Main.java
@@ -1,53 +1,75 @@
+import java.util.Scanner;
+
 import static java.lang.System.*;
 
-class Express {
-    float x;
-    float a;
-    float b;
+class House {
+    String adress;
+    int floor;
+    int krooms;
+    float area;
 
-    Express(float x) {
-        this.x = x;
+    House(String adress, int floor, int krooms, float area) {
+        this.adress = adress;
+        this.floor = floor;
+        this.krooms = krooms;
+        this.area = area;
     }
 
-    Express(float a, float b) {
-        this.a = a;
-        this.b = b;
-    }
-
-    Express(float x, float a, float b) {
-        this.x = x;
-        this.a = a;
-        this.b = b;
-    }
-
-    public float chet1() {
-        return 3 * this.x + 5;
-    }
-
-    public String chet2() {
-        if ((this.a - this.b) != 0) {
-            String rez = String.valueOf((this.a + this.b) / (this.a - this.b));
-            return rez;
-        }
-        return "Error";
-    }
-
-    public String chet3() {
-        if (this.b != 0) {
-            String rez = String.valueOf((this.a * this.x) / this.b);
-            return rez;
-        }
-        return "Error";
+    public void flat() {
+        out.printf("Адрес: %S, этаж: %d, кол-во комнат: %d, площадь: %f \n", this.adress, this.floor, this.krooms, this.area);
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Express X = new Express(2.1f);
-        out.println(X.chet1());
-        Express Y = new Express(2.1f, 1.1f);
-        out.println(Y.chet2());
-        Express Z = new Express(2.1f, 1.2f, 2.0f);
-        out.println(Z.chet3());
+        Scanner in = new Scanner(System.in);
+        out.print("Введите количество квартир: ");
+        int n = in.nextInt();
+        House[] dom = new House[n];
+        for (int i = 0; i < n; i++) {
+            out.print("Введите адрес: ");
+            String ad = in.next();
+            out.print("Введите этаж: ");
+            int fl = in.nextInt();
+            out.print("Введите кол-во комнат: ");
+            int kr = in.nextInt();
+            out.print("Введите площадь: ");
+            float ar = in.nextFloat();
+            dom[i] = new House(ad, fl, kr, ar);
+        }
+        int count = 0;
+        out.print("Введите число комнат для вывода соответствующих квартир: ");
+        int k = in.nextInt();
+        for (House i : dom) {
+            if (i.krooms == k) {
+                i.flat();
+                count++;
+            }
+        }
+        if (count == 0)
+            out.println("Квартир подходящих под данные критерии нет ");
+        out.println("Введите число комнат и этаж для вывода соответствующих квартир");
+        out.print("количество комнат: ");
+        int k1 = in.nextInt();
+        out.print("Этаж: ");
+        int f = in.nextInt();
+        count = 0;
+        for (House i : dom) {
+            if (i.krooms == k1 & i.floor == f) {
+                i.flat();
+                count++;
+            }
+        }
+        if (count == 0)
+            out.println("Квартир подходящих под данные критерии нет ");
+        count = 0;
+        out.print("Введите площадь для вывода квартир, площадь которых её превосходит: ");
+        float a = in.nextFloat();
+        for (House i : dom) {
+            if (i.area > a)
+                i.flat();
+        }
+        if (count == 0)
+            out.println("Квартир подходящих под данные критерии нет ");
     }
 }
